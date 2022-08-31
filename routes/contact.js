94b6b9dc8533925e33b71fffee9659a7f29c1f23
s-nishad <shohanurnishad@gmail.com>
2022-09-01 01:35:53 +0600

    ContactSetUp

--- routes/contact.js
+++ routes/contact.js
@@ -3,15 +3,46 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const path = require('path');
+const nodemailer = require('nodemailer');
 let checkAuthenticated = require('./functions.js').checkAuthenticated;
 
 // contact and feedback
 router.get('/contact', checkAuthenticated, (req, res) => {
-    res.render('contact');
-})
+  res.render('contact');
+});
 
+// send feedback to admin email
 router.post('/contact', checkAuthenticated, (req, res) => {
-    
-})
+  let name = req.body.name;
+  let clint_mail = req.body.email;
+  let contact = req.body.contact;
+  let message = req.body.message;
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'shohanurnishad@gmail.com',
+      pass: 'ufqknbfztsfozopz',
+    },
+  });
+
+  let mailOptions = {
+    from: '"' + name + '" <' + clint_mail + '>',
+    to: 'nishad0788@gmail.com',
+    subject: 'Feedback from ' + name,
+    text: 'Contact: ' + contact + '\n\nMessage: ' + message,
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err);
+      req.flash('error', 'Something went wrong');
+    } else {
+      console.log('Email sent: ' + info.response);
+      req.flash('success', 'Email sent successfully');
+      res.redirect('/contact');
+    }
+  });
+});
 
 module.exports = router;
