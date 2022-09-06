24326c53268e36204c00443eec7cdfd81a09598f
Shohanur Nishad <60940498+s-nishad@users.noreply.github.com>
2022-09-06 13:43:01 +0600

    Update db.js

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 try {
   (async () => {
     await mongoose.connect(
-      'mongodb://localhost:27017/project',
+      'mongodb+srv://project:PcmosZ8yRVVfxry8@cluster0.ikebn.mongodb.net/?retryWrites=true&w=majority',
       { useNewUrlParser: true, useUnifiedTopology: true }
     );
     console.log('MongoDB connected!!');
