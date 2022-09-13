6a35a6560a9adbacb37afce5c4820626a2f7e61a
s-nishad <shohanurnishad@gmail.com>
2022-09-13 15:56:15 +0600

    procfile

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 const pug = require('pug');
-const path  = require('path');
-app.set("view engine", "pug");
+const path = require('path');
+app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '../views'));
 const router = require('./routes/index.js');
 app.use(router);
 
-
 const port = process.env.PORT || 5000;
 
-app.listen(port, err => {
-    if(err) throw err;
-    console.log(`server running on port: http://localhost:${port}`);
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`server running on port: http://localhost:${port}`);
 });
